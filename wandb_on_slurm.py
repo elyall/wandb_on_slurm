c358840bfb1ae6f0d7cce92cd03a4e3fb9658e8f
Evan Lyall <evan.lyall@gmail.com>
2020-10-27 22:28:29 -0700

    gets nodes list, logs in, and assigns agents

--- wandb_on_slurm.py
+++ wandb_on_slurm.py
@@ -2,30 +2,36 @@ import wandb
 import subprocess
 import click
 import yaml
+import os
+import json
 
-wandb.login()
-wandb.init(project="wandb_on_slurm")
-
+if os.path.exists("/nfs/code/keys.json"):
+    with open("/nfs/code/keys.json") as file:
+        api_key = json.load(file)["work_account"]
+        os.environ["WANDB_API_KEY"] = api_key
 
 @click.command()
 @click.argument("config_yaml")
-@click.argument("node_list")
+@click.argument("node_list", nargs=-1)
 def run(config_yaml, node_list):
+
+    wandb.init(project="wandb_on_slurm")
+    
 
     with open(config_yaml) as file:
         config_dict = yaml.load(file, Loader=yaml.FullLoader)
-    config_dict['parameters']['epochs'] = 5
+    config_dict['program'] = '/nfs/code/examples/examples/keras/keras-cnn-fashion/train.py'
+    config_dict['parameters']['epochs']['value'] = 5
 
     sweep_id = wandb.sweep(config_dict)
-
+    
     for node in node_list:
-        subprocess.run(['srun',
+        subprocess.Popen(['srun',
                         '--nodes=1',
                         '--ntasks=1',
                         '-w',
                         node,
-                        'wandb',
-                        'agent',
+                        'start-agent.sh',
                         sweep_id])
 
 
