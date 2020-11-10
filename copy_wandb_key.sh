767ade4eee841b74081763d0d27dab9b9035282c
Evan Lyall <evan.lyall@gmail.com>
2020-11-10 10:58:56 -0800

    remove hardcoded path

--- copy_wandb_key.sh
+++ copy_wandb_key.sh
@@ -4,9 +4,9 @@
 python - << EOF
 import netrc
 import json
-nrc = netrc.netrc('/home/ec2-user/.netrc')
-key = {'work_account': nrc.authenticators('api.wandb.ai')[2]}
-with open('/nfs/code/keys.json', 'w') as fp:
+nrc = netrc.netrc()
+key = {'work_account': nrc.authenticators('api.wandb.ai')[2]} # allow for multiple keys
+with open('keys.json', 'w') as fp:
     json.dump(key, fp)
 EOF
-chmod 600 /nfs/code/keys.json
+chmod 600 keys.json
