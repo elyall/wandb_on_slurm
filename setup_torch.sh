c01c8f07c59d23a4fc6ee54dc8c6ec545231ce96
Evan Lyall <evan.lyall@gmail.com>
2020-11-09 21:26:19 -0800

    separate aws setup from examples

---
+++ setup_torch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /nfs/code # change to accessible directory
+
+# download and install conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh # install in /nfs/code/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+conda update -n base -c defaults conda -y # update conda
+
+# create virtual environment and install required dependencies
+conda create -n 'torch_example' -y
+conda activate torch_example
+conda install pytorch torchvision -c pytorch -y
+pip install --upgrade wandb
+# pip install --upgrade git+git://github.com/wandb/client.git # install latest development version of wandb
